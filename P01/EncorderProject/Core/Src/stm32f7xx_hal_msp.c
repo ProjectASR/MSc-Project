d4f557e35977ccd1ac19f0e65e1ff6c4f361ade3
Ashen Rodrigo <128624603+ProjectASR@users.noreply.github.com>
2025-04-07 21:01:09 +0530

    Added MicroSeconds Timer

--- P01/EncorderProject/Core/Src/stm32f7xx_hal_msp.c
+++ P01/EncorderProject/Core/Src/stm32f7xx_hal_msp.c
@@ -219,7 +219,18 @@ void HAL_TIM_Encoder_MspInit(TIM_HandleTypeDef* htim_encoder)
   */
 void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
 {
-  if(htim_base->Instance==TIM13)
+  if(htim_base->Instance==TIM2)
+  {
+    /* USER CODE BEGIN TIM2_MspInit 0 */
+
+    /* USER CODE END TIM2_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_TIM2_CLK_ENABLE();
+    /* USER CODE BEGIN TIM2_MspInit 1 */
+
+    /* USER CODE END TIM2_MspInit 1 */
+  }
+  else if(htim_base->Instance==TIM13)
   {
     /* USER CODE BEGIN TIM13_MspInit 0 */
 
@@ -232,7 +243,6 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
     /* USER CODE BEGIN TIM13_MspInit 1 */
 
     /* USER CODE END TIM13_MspInit 1 */
-
   }
 
 }
@@ -296,7 +306,18 @@ void HAL_TIM_Encoder_MspDeInit(TIM_HandleTypeDef* htim_encoder)
   */
 void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* htim_base)
 {
-  if(htim_base->Instance==TIM13)
+  if(htim_base->Instance==TIM2)
+  {
+    /* USER CODE BEGIN TIM2_MspDeInit 0 */
+
+    /* USER CODE END TIM2_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_TIM2_CLK_DISABLE();
+    /* USER CODE BEGIN TIM2_MspDeInit 1 */
+
+    /* USER CODE END TIM2_MspDeInit 1 */
+  }
+  else if(htim_base->Instance==TIM13)
   {
     /* USER CODE BEGIN TIM13_MspDeInit 0 */
 
