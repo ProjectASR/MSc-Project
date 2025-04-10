c54abb045ae2de8a1ed87ddb5f0fe9e78bea6f94
Ashen Rodrigo <128624603+ProjectASR@users.noreply.github.com>
2025-04-11 00:48:12 +0530

    Inputs Configured Final.

--- P01/EncorderProject/Core/Src/stm32f7xx_it.c
+++ P01/EncorderProject/Core/Src/stm32f7xx_it.c
@@ -65,8 +65,6 @@ void SDTimer_Handler(void)
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
-extern TIM_HandleTypeDef htim1;
-extern TIM_HandleTypeDef htim4;
 extern TIM_HandleTypeDef htim13;
 /* USER CODE BEGIN EV */
 
@@ -210,39 +208,6 @@ void SysTick_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32f7xx.s).                    */
 /******************************************************************************/
-
-/**
-  * @brief This function handles TIM1 update interrupt and TIM10 global interrupt.
-  */
-void TIM1_UP_TIM10_IRQHandler(void)
-{
-  /* USER CODE BEGIN TIM1_UP_TIM10_IRQn 0 */
-
-  /* USER CODE END TIM1_UP_TIM10_IRQn 0 */
-  HAL_TIM_IRQHandler(&htim1);
-  /* USER CODE BEGIN TIM1_UP_TIM10_IRQn 1 */
-
-  /* USER CODE END TIM1_UP_TIM10_IRQn 1 */
-}
-
-/**
-  * @brief This function handles TIM4 global interrupt.
-  */
-void TIM4_IRQHandler(void)
-{
-  /* USER CODE BEGIN TIM4_IRQn 0 */
-	  FatFsCnt++;
-	  if(FatFsCnt >= 10)
-	  {
-	    FatFsCnt = 0;
-	    SDTimer_Handler();
-	  }
-  /* USER CODE END TIM4_IRQn 0 */
-  HAL_TIM_IRQHandler(&htim4);
-  /* USER CODE BEGIN TIM4_IRQn 1 */
-
-  /* USER CODE END TIM4_IRQn 1 */
-}
 
 /**
   * @brief This function handles TIM8 update interrupt and TIM13 global interrupt.
